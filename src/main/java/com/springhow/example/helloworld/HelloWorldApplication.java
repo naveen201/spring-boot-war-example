73768fb79a5df49a7b15ca61a4b5bae319c61f64
naveen201 <42841119+naveen201@users.noreply.github.com>
2023-08-17 09:45:52 +0530

    Update HelloWorldApplication.java
    for checking

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "5. please visit my website learning-ocean.com";
+        return "finally successfully";
     }
 
 }
